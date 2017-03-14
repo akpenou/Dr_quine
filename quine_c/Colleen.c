20f2aca6e0ef525440c0c8992a3440a26c85fa16
Alex KPENOU <akpenou@e1r12p7.42.fr>
2017-03-14 12:34:11 +0100

    [UPD] fix colleen et grace

--- quine_c/Colleen.c
+++ quine_c/Colleen.c
@@ -14,8 +14,9 @@ int main()
 	/*
 		Second commentaire
 	*/
-	const char *s = "#include <stdio.h>%c%c/*%c%cPremier commentaire%c*/%c%cstatic int ft_strlen(char *s)%c{%c%creturn (s && *s ? 1 + ft_strlen(++s) : 0);%c}%c%cint main()%c{%c%c/*%c%c%cSecond commentaire%c%c*/%c%c%cconst char *s = %c%s%c;%c%cif (ft_strlen((char *)s))%c%c%cprintf(s, 10, 10, 10, 9, 10, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 10, 9, 34, s, 34, 10, 9, 10, 9, 9, 10, 9, 10);%c%creturn (0);%c}";
+
+	const char *s = "#include <stdio.h>%c%c/*%c%cPremier commentaire%c*/%c%cstatic int ft_strlen(char *s)%c{%c%creturn (s && *s ? 1 + ft_strlen(++s) : 0);%c}%c%cint main()%c{%c%c/*%c%c%cSecond commentaire%c%c*/%c%c%cconst char *s = %c%s%c;%c%cif (ft_strlen((char *)s))%c%c%cprintf(s, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 10, 9, 34, s, 34, 10, 9, 10, 9, 9, 10, 9, 10, 10);%c%creturn (0);%c}%c";
 	if (ft_strlen((char *)s))
-		printf(s, 10, 10, 10, 9, 10, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 10, 9, 34, s, 34, 10, 9, 10, 9, 9, 10, 9, 10);
+		printf(s, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 10, 9, 34, s, 34, 10, 9, 10, 9, 9, 10, 9, 10, 10);
 	return (0);
 }
--- quine_c/Grace.c
+++ quine_c/Grace.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 #define VAR int argc, char **argv
-#define DOJOB if (argc != 1){ printf("Usage: ./Grace_kid");return (0);}
+#define DOJOB if (argc != 1){ printf("Usage: ./Grace_kid"); (void **)argv; return (1);}
 
 /*
 	Ce define definira le main de la fonction
 */
 
-#define FONCTION(X) int main(VAR){DOJOB FILE *fl = fopen(X, "w+");const char *s = "#include <stdio.h>%c%c#define VAR int argc, char **argv%c#define DOJOB if (argc != 1){ printf(%cUsage: ./Grace_kid%c);return (0);}%c%c/*%c%cCe define definira le main de la fonction%c*/%c%c#define FONCTION(X) int main(VAR){DOJOB FILE *fl = fopen(X, %cw+%c);const char *s = %c%s%c;printf(s, 10, 10, 10, 34, 34, 10, 10, 10, 9, 10, 10, 10, 34, 34, 34, s, 34, 10, 34, 34, 10);fclose(fl);return(0);}%cFONCTION(%cGrace_kid.c%c)%c";printf(s, 10, 10, 10, 34, 34, 10, 10, 10, 9, 10, 10, 10, 34, 34, 34, s, 34, 10, 34, 34, 10);fclose(fl);return(0);}
-
+#define FONCTION(X) int main(VAR){DOJOB FILE *fl = fopen(X, "w+");const char *s = "#include <stdio.h>%c%c#define VAR int argc, char **argv%c#define DOJOB if (argc != 1){ printf(%cUsage: ./Grace_kid%c); (void **)argv; return (1);}%c%c/*%c%cCe define definira le main de la fonction%c*/%c%c#define FONCTION(X) int main(VAR){DOJOB FILE *fl = fopen(X, %cw+%c);const char *s = %c%s%c;fprintf(fl, s, 10, 10, 10, 34, 34, 10, 10, 10, 9, 10, 10, 10, 34, 34, 34, s, 34, 10, 34, 34, 10);fclose(fl);return(0);}%cFONCTION(%cGrace_kid.c%c)%c";fprintf(fl, s, 10, 10, 10, 34, 34, 10, 10, 10, 9, 10, 10, 10, 34, 34, 34, s, 34, 10, 34, 34, 10);fclose(fl);return(0);}
 FONCTION("Grace_kid.c")
